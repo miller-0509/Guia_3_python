7482fc72007d9170969e00c5f44c873afa8a89de
miller-0509 <caperamiller5@gmail.com>
2026-03-06 15:26:44 -0500

    Update 05_produccion.py

--- 05_produccion.py
+++ 05_produccion.py
@@ -24,3 +24,4 @@ if __name__ == '__main__':
     debug_mode = os.getenv("FLASK_DEBUG", "False").lower() == "true"
     port = int(os.getenv("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=debug_mode)
+    
